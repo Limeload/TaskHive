ca776c4652caeced4f5c47895072d30dc5f53dfa
Shraddha Rao <shraddharaom@gmail.com>
2023-03-03 14:35:08 -0600

    rails db:migrate for deploying

--- db/migrate/20230303194556_create_comments.rb
+++ db/migrate/20230303194556_create_comments.rb
@@ -4,7 +4,6 @@ class CreateComments < ActiveRecord::Migration[7.0]
       t.text :content
       t.belongs_to :user, null: false, foreign_key: true
       t.belongs_to :task, null: false, foreign_key: true
-      t.belongs_to :project, null: false, foreign_key: true
 
       t.timestamps
     end
