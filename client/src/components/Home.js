dcd5c3a0ddb2a1620aef62982c78ca1631e98a4f
Shraddha Rao <shraddharaom@gmail.com>
2023-03-09 02:09:20 -0600

    fixed error by replacing useHistory and added styling to home.js,login and signup component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,16 +1,27 @@
 import React from "react";
 import ProjectList from "./ProjectList";
+import { Container, Row, Col } from "react-bootstrap";
 import { Link } from 'react-router-dom';
 import { Button} from 'react-bootstrap';
+import home from '../images/home.png';
 
 
 function Home() {
     return (
-        <div className='home'>
-            <h1>Welcome to TaskHive</h1>
-            <Link to="/login"><Button variant="warning">Log in / Register</Button></Link>
+        <Container>
+      <Row className="my-4">
+        <Col>
+          <h1>Welcome to TaskHive! </h1>
+          <p className="lead">
+            TaskHive allows you to manage your tasks easily and efficiently.
+          </p>
+          <img className="home-img" src={home} alt={home} />
+         <p>Manage your tasks like a Pro!</p>
+          <Link to="/login"><Button variant="primary">Log in / Register</Button></Link>
             <ProjectList />
-        </div>
+        </Col>
+      </Row>
+    </Container>
     )
 }
 
--- client/src/components/ProjectPage.js
+++ client/src/components/ProjectPage.js
@@ -1,76 +1,95 @@
-import React, {useState, useEffect} from "react"
-import ProjectPageTaskList from "./ProjectPageTaskList"
+import React, { useState } from "react";
+import ProjectPageTaskList from "./ProjectPageTaskList";
 
-function ProjectPage({user, onAddNewTask}) {
+function ProjectPage({ onAddNewTask }) {
+  const taskInput = {
+    title: "",
+    description: "",
+    deadline: "",
+    priority: 0,
+    completed: false,
+  };
+  const [taskData, setTaskData] = useState(taskInput);
+  const [addNewTasks, setAddNewTasks] = useState([]);
 
-    const taskInput = {title: "", description: "", deadline:"", priority: 0, completed: false}
-    const [taskData, setTaskData] = useState(taskInput)
-    const [addNewTasks, setaddNewTasks] = useState([])
+  function handleTaskData(e) {
+    const { name, value } = e.target;
+    setTaskData({ ...taskData, [name]: value });
+  }
 
-    // useEffect(() => {
-    //     fetch(`/users/1`)
-    //         .then(res => res.json())
-    //         .then(projectData => setProject(projectData))
-    //   }, [])
+  function handleSubmit(e) {
+    e.preventDefault();
 
-    function handleTaskData(e) {
-        const { name, value } = e.target;
-        setTaskData({ ...taskData, [name]: value })
-    }
+    const newTask = {
+      ...taskData,
+      project_id: 1,
+      user_id: 1,
+    };
 
-    function handleSubmit(e) {
-        e.preventDefault()
-        // let completionBoolean = e.target.form.completion.value
+    fetch("/tasks/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newTask),
+    })
+      .then((res) => res.json())
+      .then((task) => {
+        setAddNewTasks([...addNewTasks, task]);
+        onAddNewTask(task);
+        setTaskData(taskInput);
+      });
+  }
 
-        const newTask = {
-            title: taskData.title,
-            description: taskData.description,
-            deadline:taskData.deadline,
-            priority: taskData.priority,
-            completed: taskData.completed,
-            project_id: 1,
-            user_id: 1
-        }
-
-        fetch("/tasks/", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(newTask)
-        })
-        .then(res => res.json())
-        .then(task => {
-            // setaddNewTasks([...project.tasks, task])
-            onAddNewTask(task)
-            setTaskData(taskInput)
-        })
-    }
-
-
-
-    return (
-        <div>
-            <h1>Create new task</h1>
-            <div className="task-form">
-            <form onSubmit={handleSubmit}>
-                <input onChange={handleTaskData} type="text" value={taskData.title} name="title" placeholder="enter task..." />
-                <input onChange={handleTaskData} type="text" value={taskData.description} name="description" placeholder="enter task description..." />
-                <input onChange={handleTaskData} type="text" value={taskData.deadline} name="deadline" placeholder="enter task deadline..." />
-                <input onChange={handleTaskData} type="number" value={taskData.priority} name="priority" placeholder="priority level 1-10..." />
-                <select name="completion">
-                    <option value="true">true</option>
-                    <option value="false">false</option>
-                </select>
-                <button type="submit">Add New Task</button>
-            </form>
-            </div>
-             <div className="project-tasks">
-                <ProjectPageTaskList projectTasks={addNewTasks}/>
-             </div>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Create new task</h1>
+      <div className="task-form">
+        <form onSubmit={handleSubmit}>
+          <input
+            onChange={handleTaskData}
+            type="text"
+            value={taskData.title}
+            name="title"
+            placeholder="enter task..."
+          />
+          <input
+            onChange={handleTaskData}
+            type="text"
+            value={taskData.description}
+            name="description"
+            placeholder="enter task description..."
+          />
+          <input
+            onChange={handleTaskData}
+            type="text"
+            value={taskData.deadline}
+            name="deadline"
+            placeholder="enter task deadline..."
+          />
+          <input
+            onChange={handleTaskData}
+            type="number"
+            value={taskData.priority}
+            name="priority"
+            placeholder="priority level 1-10..."
+          />
+          <select
+            onChange={handleTaskData}
+            value={taskData.completed}
+            name="completed"
+          >
+            <option value={false}>false</option>
+            <option value={true}>true</option>
+          </select>
+          <button type="submit">Add New Task</button>
+        </form>
+      </div>
+      <div className="project-tasks">
+        <ProjectPageTaskList projectTasks={addNewTasks} />
+      </div>
+    </div>
+  );
 }
 
-
-export default ProjectPage
+export default ProjectPage;
